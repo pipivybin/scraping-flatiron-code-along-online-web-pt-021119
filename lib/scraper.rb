842ab0b69828997cd9433c4e4c4176b4bb0a8f37
Ming Duan <11203duan@gmail.com>
2019-03-27 22:42:30 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -13,7 +13,7 @@ class Scraper
   def get_courses
     self.get_page.css(".post")
   end
-  
+
   def make_courses
     self.get_courses.each do |x|
       course = Course.new
@@ -23,17 +23,15 @@ class Scraper
     end
   end
 
-  #   def print_courses
-  #   self.make_courses
-  #   Course.all.each do |course|
-  #     if course.title
-  #       puts "Title: #{course.title}"
-  #       puts "  Schedule: #{course.schedule}"
-  #       puts "  Description: #{course.description}"
-  #     end
-  #   end
-  # end
+    def print_courses
+    self.make_courses
+    Course.all.each do |course|
+      if course.title
+        puts "Title: #{course.title}"
+        puts "  Schedule: #{course.schedule}"
+        puts "  Description: #{course.description}"
+      end
+    end
+  end
 
 end
-
-Scraper.new.get_page
