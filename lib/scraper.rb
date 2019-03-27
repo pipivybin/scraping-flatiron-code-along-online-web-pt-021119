79fdb7578a3092b00ba36de2f3c3f7fe73489e90
Ming Duan <11203duan@gmail.com>
2019-03-27 22:41:10 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -18,12 +18,7 @@ class Scraper
   end
 
   def get_courses
-    self.get_page.css(".post").each do |x|
-      course = Course.new
-      course.title = x.css("h2").text
-      course.schedule = x.css(".date").text
-      course.description = x.css("p").text
-    end
+    self.get_page.css(".post")
   end
   #
   # def make_courses
