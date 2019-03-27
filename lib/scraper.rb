8a1b64531685abd0ae1ddc9fec36bc086a907d47
Ming Duan <11203duan@gmail.com>
2019-03-27 22:36:56 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -8,7 +8,12 @@ class Scraper
   def get_page
     html = open("http://learn-co-curriculum.github.io/site-for-scraping/courses")
     doc = Nokogiri::HTML(html)
-    
+    doc.css(".post").each {|x|
+      course = Course.new
+      course.title = x.css("h2").text
+      course.schedule = x.css(".date").text
+      course.description = x.css(".p").text
+    }
   end
 
   def get_courses
